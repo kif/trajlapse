09f513bededb04777b0af972fe73d2463fff2418
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2022-07-10 17:11:45 +0200

    switch to binned mode

--- stream_tilt_pan.py
+++ stream_tilt_pan.py
@@ -280,7 +280,7 @@ class Server(object):
 
     def setup_cam(self):
         self.streamout = StreamingOutput()
-        self.camera = PiCamera(resolution=self.resolution, framerate=10)  # , sensor_mode=3)
+        self.camera = PiCamera(resolution=self.resolution, framerate=10, sensor_mode=2)
         self.camera.awb_mode = "auto"
         self.camera.exposure_mode = "nightpreview"
         self.camera.start_recording(self.streamout,
